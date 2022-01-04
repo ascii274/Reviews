d1888cf37a27168386ee3e9c2c27edcdb4f56644
Joel Cabatay <ascii274@gmail.com>
2022-01-04 16:37:29 +0100

    WebRequest-WebClient app created

---
+++ WebRequest-WebClient/src/main/java/com/ascii274/webrequest/WebRequestWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.ascii274.webrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRequestWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRequestWebClientApplication.class, args);
+	}
+
+}
---
+++ WebRequest-WebClient/src/test/java/com/ascii274/webrequest/WebRequestWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascii274.webrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRequestWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
