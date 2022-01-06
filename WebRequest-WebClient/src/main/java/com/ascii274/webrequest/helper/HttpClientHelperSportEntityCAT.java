a33ce83ae231629c1fecf9a9b5e2b5901ad4eae2
Joel Cabatay <ascii274@gmail.com>
2022-01-06 15:46:58 +0100

    getData_2 with return Mono.defer created

--- WebRequest-WebClient/src/main/java/com/ascii274/webrequest/helper/HttpClientHelperSportEntityCAT.java
+++ WebRequest-WebClient/src/main/java/com/ascii274/webrequest/helper/HttpClientHelperSportEntityCAT.java
@@ -13,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.net.URL;
 import java.util.Collections;
 import java.util.function.Consumer;
 
@@ -45,12 +46,31 @@ public class HttpClientHelperSportEntityCAT {
         codecs.maxInMemorySize(30 * 1024 * 1024);
     };
 
+    /**
+     * - functional
+     * @return
+     */
     public Mono<EntitySportCAT_DTO[]> getData(){
         WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
         WebClient.RequestBodySpec bodySpec = uriSpec.uri(URI.create(propertiesConfig.getDataEntitatEsportivaCAT()));
         return bodySpec.retrieve().bodyToMono(EntitySportCAT_DTO[].class);
     }
 
+    /**
+     * - Without Mono.defer Method works
+     * - Mehtod with Mono nameMethod works too.
+     * - I need to read more Mono.
+     * @param url
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+//    @SuppressWarnings("unchecked")
+    public <T> Mono<T> getData_2(URL url, Class clazz){
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(URI.create(url.toString()));
+        return bodySpec.retrieve().bodyToMono(clazz);
+    }
 
 
 }
