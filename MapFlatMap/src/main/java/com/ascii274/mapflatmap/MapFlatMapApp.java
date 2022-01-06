d6a40902a14102b3b841a9a1b8e145a262c6c74c
Joel Cabatay <ascii274@gmail.com>
2022-01-06 20:08:27 +0100

    MapFlatMapApp.java created

---
+++ MapFlatMap/src/main/java/com/ascii274/mapflatmap/MapFlatMapApp.java
@@ -0,0 +1,11 @@
+package com.ascii274.mapflatmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFlatMapApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MapFlatMapApp.class, args);
+    }
+}
