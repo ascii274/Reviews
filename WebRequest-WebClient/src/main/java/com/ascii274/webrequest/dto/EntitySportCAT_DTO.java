2dac4e093e4dd175e1828af3e2f6ed4e9b46b1ed
Joel Cabatay <ascii274@gmail.com>
2022-01-06 15:41:45 +0100

    JsonProperty added

--- WebRequest-WebClient/src/main/java/com/ascii274/webrequest/dto/EntitySportCAT_DTO.java
+++ WebRequest-WebClient/src/main/java/com/ascii274/webrequest/dto/EntitySportCAT_DTO.java
@@ -5,15 +5,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EntitySportCAT_DTO {
-//    @JsonProperty("n_m_registre")
+    @JsonProperty("n_m_registre")
     public String n_m_registre;
+    @JsonProperty("nom_entitat")
     public String nom_entitat;
+    @JsonProperty("adre_a")
     public String adre_a;
+    @JsonProperty("cp")
     public String cp;
+    @JsonProperty("municipi")
     public String municipi;
+    @JsonProperty("comarca")
     public String comarca;
+    @JsonProperty("tel_fon_fax")
     public String tel_fon_fax;
+    @JsonProperty("tipus_entitat")
     public String tipus_entitat;
+    @JsonProperty("modalitats")
     public String modalitats;
+    @JsonProperty("correu_electr_nic")
     public String correu_electr_nic;
+
 }
