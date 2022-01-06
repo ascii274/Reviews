4e00da8d8c68d4498b8a891a64b1d60b4eca2444
Joel Cabatay <ascii274@gmail.com>
2022-01-06 15:48:21 +0100

    Endpoint sports-entities-3 created

--- WebRequest-WebClient/src/main/java/com/ascii274/webrequest/controller/WebRequestController.java
+++ WebRequest-WebClient/src/main/java/com/ascii274/webrequest/controller/WebRequestController.java
@@ -2,6 +2,7 @@ package com.ascii274.webrequest.controller;
 
 import com.ascii274.webrequest.config.PropertiesConfig;
 import com.ascii274.webrequest.dto.EntitySportCAT_DTO;
+import com.ascii274.webrequest.dto.Root_DTO;
 import com.ascii274.webrequest.helper.HttpClientHelperSportEntityCAT;
 import com.ascii274.webrequest.helper.HttpClientHelperStarWars;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +23,10 @@ public class WebRequestController {
     HttpClientHelperStarWars httpClientHelperStarWars;
     @Autowired
     HttpClientHelperSportEntityCAT httpClientHelperSportEntityCAT;
-
     @Autowired
     PropertiesConfig propertiesConfig;
+    @Autowired
+    Root_DTO root_dto;
 
     @GetMapping(value="/test")
     public String test(){
@@ -41,10 +43,47 @@ public class WebRequestController {
         }
     }
 
-
+    /**
+     * - Getting file.json and show all data as array[Entity_DTO]
+     * - Data to show is SportsEntities in Catalonia.
+     * @return
+     */
     @GetMapping(value="/sports-entities")
-    public Mono<EntitySportCAT_DTO[]> getSportEntity(){
+    public Mono<EntitySportCAT_DTO[]> getSportEntityV1(){
         return httpClientHelperSportEntityCAT.getData();
+    }
+
+    @GetMapping(value="/sports-entities-2")
+    public Mono<Root_DTO> getSportEntityV2(){
+        Mono<EntitySportCAT_DTO[]> response = httpClientHelperSportEntityCAT.getData();
+        return response.flatMap(
+                dto ->{
+                    root_dto.setResults(dto);
+                    root_dto.setCount(dto.length);
+                    return Mono.just(root_dto);
+                });
+    }
+
+    /** My Question:
+     * - public Mono<Root_DTO<EntitySportCAT_DTO>> getSportEntityV3() doesn't work
+     * - More see about Mono
+     *
+     * @return
+     */
+    @GetMapping(value="/sports-entities-3")
+//    public Mono<Root_DTO<EntitySportCAT_DTO>> getSportEntityV3(){
+    public Mono getSportEntityV3(){
+        try {
+            Mono<EntitySportCAT_DTO[]> response = httpClientHelperSportEntityCAT.getData_2(new URL(propertiesConfig.getDataEntitatEsportivaCAT()),EntitySportCAT_DTO[].class);
+            return  response.flatMap( dto -> {
+                root_dto.setCount(dto.length);
+                root_dto.setResults(dto);
+                return Mono.just(root_dto);
+            });
+
+        } catch (MalformedURLException e) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Resource not found", e);
+        }
 
     }
 
