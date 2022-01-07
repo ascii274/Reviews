97bc6053dbb7fa22beebc1eb75b3e3d5673c644f
Joel Cabatay <ascii274@gmail.com>
2022-01-07 14:40:50 +0100

    Controller created

--- MapFlatMap/src/main/java/com/ascii274/mapflatmap/controller/MapFlatMapController.java
+++ MapFlatMap/src/main/java/com/ascii274/mapflatmap/controller/MapFlatMapController.java
@@ -1,13 +1,10 @@
 package com.ascii274.mapflatmap.controller;
 
-import org.junit.Test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,7 +18,7 @@ source https://www.baeldung.com/java-difference-map-and-flatmap
 @RestController
 @RequestMapping(value="/v1")
 
-public class MapController {
+public class MapFlatMapController {
 
     @GetMapping(value="/test-map")
     public String test(){
@@ -40,7 +37,9 @@ public class MapController {
         Optional<String> str = Optional.of("test");
 
         //str2
-        // pretty cumbersome better use fltamap( algo engoroso )
+        // pretty cumbersome better use fltamap( algo engoroso usar flatmap)
+        //estructura anidad Optional<Optional<String>>
+
         assertEquals(Optional.of(Optional.of("STRING")),
             Optional
                 .of("string")
@@ -50,7 +49,6 @@ public class MapController {
                 "\n" + str ;
     }
 
-
     @GetMapping(value = "/map-in-optional")
     public Optional<String> testMap_2(){
         Optional<String> s = Optional.of("test");
@@ -58,14 +56,46 @@ public class MapController {
         return s;
     }
 
-//    @Test
+    @GetMapping(value = "/flatmap-in-optional")
+    public Optional<String> testFlatMap(){
+        Optional<String> s = Optional.of("test");
+        assertEquals(Optional.of("STRING") ,Optional
+                .of("string")
+                .flatMap(s2 -> Optional.of("STRING"))
+        );
+        return s;
+    }
+
+    /**
+     * Streams
+     */
+
     @GetMapping(value = "/map-stream")
     public List<String> convertStringToUpperCaseStreams() {
         List<String> collected = Stream.of("a", "b", "hello") // Stream of String
                 .map(String::toUpperCase) // Returns a stream consisting of the results of applying the given function to the elements of this stream.
                 .collect(Collectors.toList());
         assertEquals(asList("A", "B", "HELLO"), collected);
-        return collected;
+        return collected; //return ["A","B","HELLO"]
+    }
+
+    @GetMapping(value = "/map-stream-2")
+    public List<List<String>>  listOfList(){
+        List<List<String>> list = Arrays.asList(
+          Arrays.asList("a","b"),
+          Arrays.asList("b","c"));
+          return list; // return [ ["a","b"] ["c","d"]]
+    }
+
+    @GetMapping(value = "/flatmap-stream")
+    public List<String> listOfListFltMap(){
+//        List<List<String>> list = listOfList();
+        List<List<String>> list = Arrays.asList(
+                Arrays.asList("1","2"),
+                Arrays.asList("3","4"));
+        return list.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList()); // return flattened ["1","2","3","4"]
     }
 
 
