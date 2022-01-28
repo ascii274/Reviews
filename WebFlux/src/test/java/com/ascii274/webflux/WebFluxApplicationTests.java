5ddc1b655315e861282b72fcec0cc73b02a403f9
Joel Cabatay <ascii274@gmail.com>
2022-01-28 23:49:55 +0100

    WebFlux Initial commit

---
+++ WebFlux/src/test/java/com/ascii274/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascii274.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
