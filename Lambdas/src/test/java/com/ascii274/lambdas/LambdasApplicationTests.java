d81d028dedf87c9fd58a20d8e27be166ca66a4bf
Joel Cabatay <ascii274@gmail.com>
2022-03-24 17:54:00 +0100

    Lambda Rest created

---
+++ Lambdas/src/test/java/com/ascii274/lambdas/LambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascii274.lambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
